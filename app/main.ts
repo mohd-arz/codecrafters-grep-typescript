9ab34d2d0c77a654b90805988e5fc3a478fde540
mohd-arz <mohammedarsh75@gmail.com>
2024-08-19 20:07:44 +0530

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,10 +1,23 @@
+export {};
+
 const args = process.argv;
 const pattern = args[3];
 
 const inputLine: string = await Bun.stdin.text();
 
 function matchPattern(inputLine: string, pattern: string): boolean {
-  if (pattern.length === 1) {
+  if(pattern == "\\d"){
+    let found = false;
+    for(let i=0;i<inputLine.length;i++){
+      let ascii = inputLine.charCodeAt(i);
+      if(ascii >= 48 && ascii <=59){
+        found = true;
+        break;
+      }
+    }
+    return found;
+  }
+  else if (pattern.length === 1) {
     return inputLine.includes(pattern);
   } else {
     throw new Error(`Unhandled pattern: ${pattern}`);
@@ -21,7 +34,9 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 if (matchPattern(inputLine, pattern)) {
+  console.log('matched');
   process.exit(0);
 } else {
+  console.log('not matched');
   process.exit(1);
 }
