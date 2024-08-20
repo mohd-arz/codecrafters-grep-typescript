b83c9b160951c3c40960c9f290eb5f71bfac6909
mohd-arz <mohammedarsh75@gmail.com>
2024-08-20 19:45:50 +0530

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -5,12 +5,26 @@ const pattern = args[3];
 
 const inputLine: string = await Bun.stdin.text();
 
+function getW(ascii:number):boolean{
+  if((ascii >= 48 && ascii <=59)||(ascii>=65&&ascii<=90)||(ascii>=97&&ascii<=122)||ascii==95){
+   return true;
+  }
+  return false;
+}
+
+function getD(ascii:number):boolean{
+  if(ascii >= 48 && ascii <=59){
+    return true; 
+  }
+  return false;
+}
+
 function matchPattern(inputLine: string, pattern: string): boolean {
   if(pattern == "\\d"){
     let found = false;
     for(let i=0;i<inputLine.length;i++){
       let ascii = inputLine.charCodeAt(i);
-      if(ascii >= 48 && ascii <=59){
+      if(getD(ascii)){
         found = true;
         break;
       }
@@ -20,7 +34,7 @@ function matchPattern(inputLine: string, pattern: string): boolean {
     let found = false;
     for(let i=0;i<inputLine.length;i++){
       let ascii = inputLine.charCodeAt(i);
-      if((ascii >= 48 && ascii <=59)||(ascii>=65&&ascii<=90)||(ascii>=97&&ascii<=122)||ascii==95){
+      if(getW(ascii)){
         found = true;
         break;
       }
@@ -53,7 +67,57 @@ function matchPattern(inputLine: string, pattern: string): boolean {
   }
   else if (pattern.length === 1) {
     return inputLine.includes(pattern);
-  } else {
+  }else if(pattern.length > 1){
+    console.log('input ',inputLine.trim().length,inputLine.length);
+    let compare = [];
+    for(let i=0;i<pattern.length;i++){
+      if(pattern[i]=='\\'){
+        if(pattern[i+1]=='d'){
+          compare.push('\\d');
+        }
+        else if(pattern[i+1]=='w'){
+          compare.push('\\w');
+        }
+        i+=1;
+        continue;
+      }else{
+        compare.push(pattern[i]);
+      }
+    }
+    // console.log('pattern ',compare)
+    let compareCount = 0;
+    for(let i=0;i<inputLine.length;i++){
+      console.log('comparing against ',compare[compareCount],inputLine[i])
+      if(compareCount == compare.length){
+        return true;
+      }
+      if(compare[compareCount]=='\\w'){
+        const ascii = inputLine.charCodeAt(i);
+        if(getW(ascii)){
+          compareCount++;
+        }else{
+          i-=1;
+          compareCount = 0;
+          continue
+        }
+      }else if(compare[compareCount]=='\\d'){
+        const ascii = inputLine.charCodeAt(i);
+        if(getD(ascii)){
+          compareCount++;
+        }else{
+          compareCount = 0;
+          continue
+        }
+      }else if(inputLine[i]==compare[compareCount]){
+        compareCount++;
+      }else{
+        compareCount = 0;
+      }
+    }
+    if(compareCount == compare.length)return true
+    return false;
+  } 
+  else {
     throw new Error(`Unhandled pattern: ${pattern}`);
   }
 }
